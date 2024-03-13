7ac72ab7f54e61a19925849795ccbe97091c8243
LisbethCP <lici2023@gmail.com>
2024-03-13 00:17:48 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+/*
+//lert("Hola este es mi Javascript");
+let nombre1 = "Lisbeth";
+
+console.log(nombre1);
+
+
+let contenidoDeTitulo = "HUELLITAS";
+
+
+//let titulo = document.querySelector(".logo");
+//console.log(titulo);
+
+
+let nombre = "Ani";
+let ciudad = "Lima";
+let gusto = "helado";
+
+let parrafo = document.querySelector(".parrafo1");
+
+function cambiarTexto(nombre, ciudad, gusto){
+  let contenido = `Lorem, ipsum ${nombre}, sit amet consectetur ${ciudad},  elit. Officiis, culpa! Laborum maiores esse ${gusto}; accusantium officia, harum quod consequuntur laudantium! Eveniet doloribus modi nostrum sint accusantium harum eaque quam iusto`; 
+
+
+
+  return contenido;
+}
+
+parrafo.innerText = cambiarTexto(nombre, ciudad, gusto);
+*/
